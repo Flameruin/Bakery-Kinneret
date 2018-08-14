d68f94c70ce79d976005746152c4d9cb7d9d5c9e
Flameruin <flame.dekel@gmail.com>
2018-08-14 11:59:00 +0300

    Add files via upload

---
+++ Code/test5/test5/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using test5.Models;
+
+namespace test5
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_OnPostAuthenticateRequest(object sender, EventArgs e)
+        {
+            var user = HttpContext.Current.User;
+            SimpleConfigurationPrincipal.SetAuthenticatedPrincipal(user);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.IsSecureConnection.Equals(false) &&
+                HttpContext.Current.Request.IsLocal.Equals(false))
+            {
+                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] +
+                                   HttpContext.Current.Request.RawUrl);
+            }
+        }
+    }
+}
